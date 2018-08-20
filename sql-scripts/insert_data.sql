5f180775b46acc5ffa9dde9385a300eaf7d031b2
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-08-20 12:30:55 +0200

    Create insert_data.sql

---
+++ sql-scripts/insert_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO upload_images VALUES (1,"Qingye Jiang","clouds1.jpg","2015-01-31 04:21:11"),
+        (2,"Qingye Jiang","clouds2.jpg","2015-01-31 04:21:15"),
+        (3,"Qingye Jiang","clouds3.jpg","2015-01-31 04:21:20"),
+        (4,"Qingye Jiang","clouds4.jpg","2015-01-31 04:21:25"),
+        (5,"Qingye Jiang","clouds5.jpg","2015-01-31 04:24:26"),
+        (6,"Qingye Jiang","clouds6.jpg","2015-01-31 04:24:30"),
+        (7,"Qingye Jiang","clouds7.jpg","2015-01-31 04:24:34"),
+        (8,"Qingye Jiang","clouds8.jpg","2015-01-31 04:24:38"),
+        (9,"Qingye Jiang","clouds9.jpg","2015-01-31 04:25:49"),
+        (10,"Qingye Jiang","clouds10.jpg","2015-01-31 04:25:53")
